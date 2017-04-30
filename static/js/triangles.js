7da7050ddd134d92f2578d296091baccd8155578
shinson <sonia.hinson@gmail.com>
2017-04-29 20:31:22 -0400

    updating triangle background style

--- static/js/triangles.js
+++ static/js/triangles.js
@@ -1,26 +1,27 @@
-var colorsOdd = ["#152934", "#128EA8", "#3C6B75", "#559AA8", "none"]
+var colorsOdd = ["#152934", "#3C6B75", "#559AA8", "none", "none"]
 
-var colorsEven = ["#45818E", "#7890CD", "none"]
+var colorsEven = ["#45818E", "#7890CD", "none", "none"]
 
 function Draw(){
 
-var w = 1650;
+var w = window.innerWidth;
 var h = window.innerHeight;
 var triangle = d3.symbol().type(d3.symbolTriangle);
-var spaceW = 24,
+var spaceW = 76,
   spaceWOpp = 0,
-  spaceH = 30,
-  spaceHOpp = 16,
-  spaceHBtwn = 42,
-  size = 1050;
+  spaceH = 150,
+  spaceHOpp = 107,
+  spaceHBtwn = 130,
+  size = 10050;
 
 var svg = d3.select('#triangles')
   .append('svg')
   .attr('width', w)
-  .attr('height', h);
+  .attr('height', h)
+  .attr('opacity', 0.2);
 
-rows = _.range(h / 20);
-columns = _.range(w / 24);
+rows = _.range(h / 170);
+columns = _.range(w / 75);
 
 function getColorO() {
   return colorsOdd[Math.floor(Math.random() * colorsOdd.length)];
@@ -40,16 +41,14 @@ _.forEach(rows, function(i) {
         .transition()
         .duration(4000)
         .attr("transform", "translate(" + spaceW * j  + "," + ((i * spaceHBtwn) + spaceH) + ") rotate(360)")
-        .attr('fill', getColorE)
-        .attr('opacity', 1);
+        .attr('fill', (j%4===0)? 'none':getColorE);
       
     } else {
       svg.append("path")
         .attr("class", "pathOdd")
         .attr("d", triangle.size(size))
         .attr("transform", "translate(" + (spaceW + spaceWOpp) * j + "," + ((i * spaceHBtwn) + spaceHOpp) + ") rotate(180)")
-        .attr('fill', getColorO)
-        .attr('opacity', 1);
+        .attr('fill', (j%3===0)? 'none':getColorO);
     }
   });
 });
